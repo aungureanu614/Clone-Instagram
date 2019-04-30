fd41f59d1bf176ff8a9a9c0b6186fc8abebb5e03
Ana-Maria Ungureanu <ana-maria.ungureanu@zaplabs.com>
2019-04-30 13:23:16 -0700

    change app.css to index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import LoginContainer from './components/Login/LoginContainer/LoginContainer'
-import './App.css'
+import './index.css'
 
 const App = () => <LoginContainer />
 
