28571c112945d7ea3aa47c2f5a251d3f09df7d88
Ana-Maria Ungureanu <ana-maria.ungureanu@zaplabs.com>
2019-05-01 18:29:56 -0700

    add link to login page when clicking on sign in

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './Header.module.css'
+import { Link } from 'react-router-dom'
 
-const Header = ({ option, text, icon }) => (
+const Header = ({ text, icon, option }) => (
   <div className={styles.headerContainer}>
     <div className={styles.left}>
       {icon}
       <span className={styles.text}>{text}</span>
     </div>
-    <div className={styles.right}>{option}</div>
+    {option && (
+      <Link className={styles.right} to={'/login'}>
+        {option}
+      </Link>
+    )}
   </div>
 )
 
 export default Header
 
 Header.propTypes = {
-  option: PropTypes.string.isRequired,
+  option: PropTypes.string,
   text: PropTypes.string.isRequired,
-  icon: PropTypes.any, // Give icon a proper type
+  icon: PropTypes.element,
+}
+
+Header.defaultProps = {
+  option: '',
 }
