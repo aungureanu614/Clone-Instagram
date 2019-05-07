009975775912db7cae9e91342aaedc42cd75328b
Ryan Lee <ryan.lee@virtahealth.com>
2019-05-07 15:50:57 -0700

    TypeScript linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,32 @@
 module.exports =  {
   parser:  '@typescript-eslint/parser',
   extends:  [
+    'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
- parserOptions:  {
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier',
+  ],
+  env: {
+    'browser': true,
+    'jest': true
+  },
+  parserOptions:  {
     ecmaVersion:  2018,
     sourceType:  'module',
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    }
+  },
+  rules: {
+    'prettier/prettier': 'error'
   },
 }
