b690602beba8d1a42766ed7272a66a6016ce1957
Ana-Maria Ungureanu <ana-maria.ungureanu@zaplabs.com>
2019-05-07 10:37:10 -0700

    add typescript and convert files

--- src/components/Login/LoginContainer/LoginContainer.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import LoginContainer from './LoginContainer'
-
-test('LoginContainer renders', () => {
-  const component = renderer.create(<LoginContainer />)
-  const tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
-})
